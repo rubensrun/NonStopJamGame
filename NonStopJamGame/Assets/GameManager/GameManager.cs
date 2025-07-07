ec3a05c1f1f26d68dee22eae7faf3c9f083d3054
KPorter417 <K06E2003@Gmail.Com>
2025-07-07 21:26:40 +0100

    Scene Cycling

--- NonStopJamGame/Assets/GameManager/GameManager.cs
+++ NonStopJamGame/Assets/GameManager/GameManager.cs
@@ -7,11 +7,30 @@ public class GameManager : MonoBehaviour
     public static float speed;
     public static List<ObjectMover> activeObjectMovers = new List<ObjectMover>();
     public ObjectPool CarLeftPool;
+    public ObjectPool ScenePool;
 
     private void Start()
     {
         speed = 30f;
         Spawn();
+        GameObject obj = ScenePool.GetFromPool();
+        if (obj != null)
+        {
+            obj.transform.position = new Vector3(0, 0, 0);
+            obj.transform.rotation = Quaternion.identity;
+            ObjectMover objM = obj.GetComponent<ObjectMover>();
+            objM.SetSpeed();
+            activeObjectMovers.Add(objM);
+        }
+        GameObject obj2 = ScenePool.GetFromPool();
+        if (obj2 != null)
+        {
+            obj2.transform.position = new Vector3(24.45f, 0, 0);
+            obj2.transform.rotation = Quaternion.identity;
+            ObjectMover objM2 = obj2.GetComponent<ObjectMover>();
+            objM2.SetSpeed();
+            activeObjectMovers.Add(objM2);
+        }
     }
 
     public static void LoseSpeed(float loss)
@@ -37,6 +56,19 @@ public class GameManager : MonoBehaviour
             activeObjectMovers.Add(objM);
         }
         StartCoroutine(Wait());
+    }
+
+    public void SpawnScene()
+    {
+        GameObject obj = ScenePool.GetFromPool();
+        if (obj != null)
+        {
+            obj.transform.position = new Vector3(24.45f, 0, 0);
+            obj.transform.rotation = Quaternion.identity;
+            ObjectMover objM = obj.GetComponent<ObjectMover>();
+            objM.SetSpeed();
+            activeObjectMovers.Add(objM);
+        }
     }
 
     private IEnumerator Wait()
